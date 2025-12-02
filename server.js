488569bd9b4094650034c8aa69871fb700e42ca1
matheuszpher <matheusrod@alu.ufc.br>
2025-12-01 22:58:42 -0300

    fix: comments

--- server.js
+++ server.js
@@ -3,7 +3,6 @@ const path = require('path');
 const app = express();
 const port = 3000;
 
-// Configura para servir o arquivo HTML no diretório atual
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'index.html'));
 });
